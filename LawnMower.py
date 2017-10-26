618ad885a027b326bc8fcca6d258d62e0dde039d
billybimbob <32024142+billybimbob@users.noreply.github.com>
2017-10-26 13:52:37 -0500

    Cleaned the code

--- LawnMower.py
+++ LawnMower.py
@@ -14,11 +14,8 @@ def lawnMower():
   for i in range(0,8):
     for j in range(0,8):
       sense.set_pixel(i,j, grassColor)
-      
-  lastX = 0
-  lastY = 7
-  loX = 0
-  loY = 7
+ 
+  loX, lastX, loY, lastY = 0, 0, 7, 7
   points = 0
   sense.set_pixel(loX, loY, [248, 0, 32])
   
@@ -27,31 +24,32 @@ def lawnMower():
     x = orient["pitch"]
     y = orient["roll"]
     
-    if (y > moveThresh and y < 180 and (loY < 7)):
+    if (y > moveThresh and y < 180 and loY < 7):
       if(sense.get_pixel(loX, loY+1)==grassColor):
         points+=1
       loY = lastY+1
-    elif(y < 360 - moveThresh and y > 180 and (loY>0)):
+    elif(y < 360 - moveThresh and y > 180 and loY > 0):
       if(sense.get_pixel(loX, loY-1)==grassColor):
         points+=1
       loY = lastY-1
-    if (x > moveThresh and x < 180 and (loX>0)):
+    if (x > moveThresh and x < 180 and loX > 0):
       if(sense.get_pixel(loX-1, loY)==grassColor):
         points+=1
       loX = lastX-1
-    elif (x < 360 - moveThresh and x > 180 and (loX<7)):
+    elif (x < 360 - moveThresh and x > 180 and loX < 7):
       if(sense.get_pixel(loX+1, loY)==grassColor):
         points+=1
       loX = lastX+1
       
-    sense.set_pixel(lastX, lastY, [2, 255, 119])
+    sense.set_pixel(lastX, lastY, [2, 252, 119])
     lastX = loX
     lastY = loY
-    sense.set_pixel(loX, loY, [255, 2, 36])
+    sense.set_pixel(loX, loY, [248, 2, 36])
     if(points == 64):
       print("You Won!")
     print(points)
     #print(x, y, temp)
     
     time.sleep(1)
+
 lawnMower()
