2819094eca48d4697574f5a474d2602304aaa874
Noah Dela Rosa <noahluvsmilkduds@gmail.com>
2017-10-26 12:43:32 -0500

    Points?

--- LawnMower.py
+++ LawnMower.py
@@ -1,6 +1,5 @@
 from sense_hat import SenseHat
 import time
-<<<<<<< HEAD
   
 # max rgb values are [248,252,248]
   
@@ -56,47 +55,3 @@ def lawnMower():
     
     time.sleep(1)
 lawnMower()
-=======
-
-sense = SenseHat()
-sense.clear()
-
-moveThresh = 30
-
-temp = sense.get_temperature()
-
-
-
-for i in range(0,8):
-  for j in range(0,8):
-    sense.set_pixel(i,j, [0,255,0])
-    
-loX = 0
-loY = 7
-sense.set_pixel(loX, loY, [255, 255, 255])
-
-while(True):
-    orient = sense.get_orientation()
-    x = orient["pitch"]
-    y = orient["roll"]
-    z = orient["yaw"]
-  
-    if (y > moveThresh and y < 180):
-        print("blah")
-        loY += 1
-    elif (y < 360 - moveThresh and y > 180):
-        print("meh")
-        loY -= 1
-    if (x > moveThresh and x < 180):
-        print("yes")
-        loX -= 1
-    elif (x < 360 - moveThresh and x > 180):
-        print("nah")
-        loX += 1
-    
-    sense.set_pixel(loX, loY, [255, 255, 255])
-    #print(x, " ", y, " ", z)
-    print("pitch: {0}        roll: {1}         yaw: {2}".format(x, y, z))
-  
-    time.sleep(1)
->>>>>>> 1e2ec8abf6741363a1c58dcbfdbadb055d619d35
