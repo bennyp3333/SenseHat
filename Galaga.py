1e3ea1322b05badc0169481f324c423236b31ea4
Noah Dela Rosa <noahluvsmilkduds@gmail.com>
2017-11-07 14:53:22 -0600

    Moving ship

--- Galaga.py
+++ Galaga.py
@@ -4,24 +4,46 @@ import time
   
 # max rgb values are [248,252,248]
 
-spaceColor = [0,0,0]
-
-spaceShip = Ship()
-sense = SenseHat()
-
-while(True):
-  command = sense.stick.get_events()
-  
-  if (command.direction == "left"):
-    print("yes")
-  elif (command.direction == "right"):
-    print("blah")
     
-  if (command.action == "pressed"):
-    
-class Ship:
+class SpaceShip:
   
   def __init__(self):
-    self.shipLocX, self.shipLocY = 0, 7
-    self.shipLastX, self.shipLastY = self.shipLastX, self.shipLastY
-    self.shipColor = [248, 252, 248]
+    self.xMin, self.xMax, self.yMin, self.yMax = 0, 7, 0, 7
+    self.locX, self.locY = int((self.xMin*2)/2), self.yMax
+    self.lastX, self.lastY = self.locX, self.locY
+    self.color = [248, 252, 248]
+    
+    
+spaceColor = [10,10,10]
+
+ship = SpaceShip()
+sense = SenseHat()
+
+for i in range(ship.xMax+1):
+  for j in range(ship.yMax+1):
+    sense.set_pixel(i, j, spaceColor)
+    
+sense.set_pixel(ship.locX, ship.locY, ship.color)
+
+while(True):
+  command = sense.stick.wait_for_event(emptybuffer=True)
+  print(command.direction)
+  if (command.direction == "left" and ship.locX > ship.xMin):
+    print("yes")
+    ship.locX-=1
+  elif (command.direction == "right" and ship.locX < ship.xMax):
+    print("blah")
+    ship.locX+=1
+  else:
+    print("nothing")
+    
+  sense.set_pixel(ship.lastX, ship.lastY, spaceColor)
+  ship.lastX, ship.lastY = ship.locX, ship.locY
+  sense.set_pixel(ship.locX, ship.locY, ship.color)
+  print("got here")
+    
+  if (command.action == "pressed"):
+    print("yes")
+    
+  time.sleep(0.4)
+    
