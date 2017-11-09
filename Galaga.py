cf045a038575c3815d7011f4fd5eb2546f464cd1
Noah Dela Rosa <noahluvsmilkduds@gmail.com>
2017-11-09 14:43:16 -0600

    stuff

--- Galaga.py
+++ Galaga.py
@@ -6,9 +6,9 @@ import time
 
 class SpaceShip:
   
-  def __init__(self, xMax, yMax, enemy):
+  def __init__(self, xStart, yStart, enemy): #use max and min to find start position
     self.enemy = enemy
-    self.locX, self.locY = int((xMax)/2), yMax
+    self.locX, self.locY = xStart, yStart
     self.lastX, self.lastY = self.locX, self.locY
     self.color = [248, 252, 248]
     
@@ -22,11 +22,12 @@ class Missile:
   def __init__(self, ship):
     if ship.enemy==False: #assume enemies will attack down
       self.modify = -1
-    else:
+    elif ship.enmey==True:
       self.modify = 1
     self.ship = ship
     self.locX, self.locY = ship.locX, ship.locY+self.modify
     self.lastX, self.lastY = self.locX, self.locY
+    
     
 def shift(sense, item, spaceColor):
   sense.set_pixel(item.lastX, item.lastY, spaceColor)
@@ -34,46 +35,62 @@ def shift(sense, item, spaceColor):
   sense.set_pixel(item.locX, item.locY, item.color)
 
 def main():      
-  spaceColor = [10,10,10]
+  spaceColor = [20,20,20]
   xMin, xMax, yMin, yMax = 0, 7, 0, 7
   
   missileList = []
-  ship = SpaceShip(xMax, yMax, False)
+  ship = SpaceShip(int((xMax)/2), yMax, False)
   sense = SenseHat()
+  
+  enemyList = []
+  for i in range(5):
+    randX, randY = random.randint(xMin+1, xMax-1), random.randint(yMin+1, yMax-2)
+    curMon = SpaceShip(randX, randY, True)
+    print "yes", curMon.locX, curMon.locY
+    #sense.set_pixel(curMon.locX, curMon.locY, [0,252,0])
+    enemyList.append(curMon)
   
   for i in range(xMax+1):
     for j in range(yMax+1):
       sense.set_pixel(i, j, spaceColor)
       
-  sense.set_pixel(ship.locX, ship.locY, ship.color)
+  #sense.set_pixel(ship.locX, ship.locY, ship.color)
+  shift(sense, ship, spaceColor)
   
-  while(True):
-    for command in sense.stick.get_events():
-    #using held breaks so far
-      print(command.direction)
-      if (command.action == "pressed" and command.direction == "left" and ship.locX > xMin):
-        print("yes")
-        ship.locX-=1
-      elif (command.action == "pressed" and command.direction == "right" and ship.locX < xMax):
-        print("blah")
-        ship.locX+=1
-      elif (command.action == "pressed" and command.direction == "up"):
-        print("yes")
-        missileList.append(ship.fire())
-        curMiss = missileList[len(missileList)-1]
-        sense.set_pixel(curMiss.locX, curMiss.locY, Missile.color)
-        
-      
-      time.sleep(0.1)
-        
+  for enemy in enemyList:
+    shift(sense, enemy, spaceColor)
     
-    for missile in missileList: #need to set bounds
-      if missile.locX-1 > xMin and missile.locX+1 < xMax:
-        missile.locY+=missile.modify
-      shift(sense, missile, spaceColor)
+  while(True):
+    for event in sense.stick.get_events(): #using held breaks so far
+      #print(command.direction)
+      if event.action == "pressed":
+        if event.direction == "left" and ship.locX > xMin:
+          print("yesl")
+          ship.locX-=1
+        elif event.direction == "right" and ship.locX < xMax:
+          print "blah"
+          ship.locX+=1
+        elif event.direction == "up":
+          print "yes"
+          missileList.append(ship.fire())
+          curMiss = missileList[len(missileList)-1]
+          sense.set_pixel(curMiss.locX, curMiss.locY, Missile.color)
+          
+      elif event.action == "held":
+        print 'held'
+        break
       
+      shift(sense,ship, spaceColor)  
+      #time.sleep(0.1)
+      
+    for missile in missileList: #need to set bounds
+      if missile.locY-1 >= yMin and missile.locY+1 <= yMax:
+        missile.locY+=missile.modify
+        shift(sense, missile, spaceColor)
+      else:
+        sense.set_pixel(missile.locX, missile.locY, spaceColor)
+        missileList.remove(missile)
         
-    shift(sense,ship, spaceColor)
     #sense.set_pixel(ship.lastX, ship.lastY, spaceColor)
     #ship.lastX, ship.lastY = ship.locX, ship.locY
     #sense.set_pixel(ship.locX, ship.locY, ship.color)
