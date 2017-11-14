73a8f207f65ed864ec72437f7c4b4ed398b49c8f
2MuchSauce <32551290+2MuchSauce@users.noreply.github.com>
2017-11-14 12:30:09 -0600

    Update frogger.py

--- frogger.py
+++ frogger.py
@@ -1,46 +1,74 @@
 from sense_hat import SenseHat
 import time
+import random
 sense = SenseHat()
 # max rgb values are [248,252,248]
+R = [255, 0, 0]  # Red
+O = [255, 128, 0]  # orange
+Y = [255, 255, 0]  # Yellow
+L = [139, 68, 16]  # Log
+
+G = [0, 255, 0]  # Green
+S = [0, 100, 0]  # Dark Green
+C = [0, 255, 255]  # Cyan
+I = [0, 128, 248]  # Light Blue, Indigo?
+
+B = [0, 0, 255]  # Blue
+P = [127, 0, 255]  # Purple
+H = [255, 0, 255]  # Hot Pink
+D = [255, 0, 127]  # Dark Red/Pink
+
+K = [0, 0, 0]  # Black
+E = [50, 50, 50]  # Grey
+W = [255, 255, 255]  # White
+bg = [
+G, G, G, G, G, G, G, G,
+G, G, G, G, G, G, G, G,
+I, I, I, I, I, I, I, I,
+I, I, I, I, I, I, I, I,
+I, I, I, I, I, I, I, I,
+I, I, I, I, I, I, I, I,
+G, G, G, G, G, G, G, G,
+G, G, G, G, G, G, G, G
+]
+class log:
+  def __init__(self, logy):
+    self.logy = logy
+    self.logx = random.randrange(-30,0)
+  def moveLog(self):
+    rand = random.randrange(1,10)
+    if self.logx > -1 and self.logx < 8:
+      if rand<6:
+        sense.set_pixel(self.logx, self.logy, L)
+      else:
+        sense.set_pixel(self.logx, self.logy, I)
+    self.logx += 1
+    if self.logx > 8:
+      self.logx = random.randrange(-30,0)
 
 def xy(x, y):
   return((y*8)+x)
 
 def frogger():
+  log1 = log(2)
+  log2 = log(3)
+  log3 = log(4)
+  log4 = log(5)
+  
   x, lastx, y, lasty = 0, 0, 7, 7
-  R = [255, 0, 0]  # Red
-  O = [255, 128, 0]  # orange
-  Y = [255, 255, 0]  # Yellow
-  L = [128, 255, 0]  # Lime?
-  
-  G = [0, 255, 0]  # Green
-  S = [0, 100, 0]  # Dark Green
-  C = [0, 255, 255]  # Cyan
-  I = [0, 128, 248]  # Light Blue, Indigo?
-  
-  B = [0, 0, 255]  # Blue
-  P = [127, 0, 255]  # Purple
-  H = [255, 0, 255]  # Hot Pink
-  D = [255, 0, 127]  # Dark Red/Pink
-  
-  K = [0, 0, 0]  # Black
-  E = [50, 50, 50]  # Grey
-  W = [255, 255, 255]  # White
-  bg = [
-  G, G, G, G, G, G, G, G,
-  G, G, G, G, G, G, G, G,
-  I, I, I, I, I, I, I, I,
-  I, I, I, I, I, I, I, I,
-  I, I, I, I, I, I, I, I,
-  I, I, I, I, I, I, I, I,
-  G, G, G, G, G, G, G, G,
-  G, G, G, G, G, G, G, G
-  ]
+
   for i in range(0,8):
     for j in range(0,8):
       sense.set_pixels(bg)
   sense.set_pixel(0, 7, P)
+  counter = 0
   while True:
+    if counter >= 50:
+      log1.moveLog()
+      log2.moveLog()
+      log3.moveLog()
+      log4.moveLog()
+      counter = 0
     for event in sense.stick.get_events():
         sense.set_pixel(x, y, P)
         if event.action == 'pressed' and event.direction == 'up' and y>0:
@@ -51,7 +79,9 @@ def frogger():
           lasty = y
           y -= 1
           sense.set_pixel(x, lasty, bg[xy(x, lasty)])
-        if event.action == 'pressed' and event.direction == 'down' and y<7 and sense.get_pixel(x, y+1) != I:
+        if event.action == 'pressed' and event.direction == 'down' and y<7:
+          if sense.get_pixel(x, y+1) == I:
+            frogger()
           lasty = y
           y += 1
           sense.set_pixel(x, lasty, bg[xy(x, lasty)])
@@ -66,5 +96,7 @@ def frogger():
             frogger()
           lastx = x
           x -= 1
-          sense.set_pixel(lastx, y, bg[xy(lastx, y)])
+        sense.set_pixel(lastx, y, bg[xy(lastx, y)])
+    time.sleep(0.01)
+    counter += 1
 frogger()
