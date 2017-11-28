30c243f14635d78d2ac1fbdbe56fb9a5bffe1bd2
bennyp3333 <bennyp3333@gmail.com>
2017-11-28 11:59:58 -0600

    changed menu

--- pictures.py
+++ pictures.py
@@ -7,15 +7,18 @@ sense = SenseHat()
 
 n = 1
 
-numPics = 1
-
+numPics = 2
+sense.clear()
+sense.load_image("pictures/picture{0}.png".format(n))
 while(True):
-    sense.clear()
-    sense.load_image("pictures/picture{0}.png".format(n))
     for event in sense.stick.get_events():
         if((event.action == 'pressed' or event.action == 'held') and event.direction == 'right'):
             n = n + 1
         if((event.action == 'pressed' or event.action == 'held') and event.direction == 'left'):
             n = n - 1
-    if(n >= numPics):
+        if(event.action == 'held' and event.direction == 'middle'):
+            exit()
+    if(n > numPics or n <= 0):
         n = 1
+    print(n)
+    sense.load_image("pictures/picture{0}.png".format(n))
