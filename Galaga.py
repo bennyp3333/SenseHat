d8d8a2769e946bbdc53ad7063822e546719e8749
Noah Dela Rosa <noahluvsmilkduds@gmail.com>
2017-11-28 14:58:48 -0600

    formating

--- Galaga.py
+++ Galaga.py
@@ -20,23 +20,22 @@ class SpaceShip:
   def fire(self, damage): #damage as parameter temporary
     return Missile(self, damage)
 
+
 class Missile:
 
   def __init__(self, ship, damage):
-    if ship.enemy==False: #assume enemies will attack down
+    self.ship = ship
+    self.damage = damage
+    if self.ship.enemy==False: #assume enemies will attack down
       self.modify = -1
       self.color = [120, 0, 248]
-      
-    elif ship.enemy==True:
+    elif self.ship.enemy==True:
       self.modify = 1
       self.color = [248, 0, 0]
-    self.damage = damage
-    self.ship = ship
     self.x_loc, self.y_loc = ship.x_loc, ship.y_loc+self.modify
     self.last_x, self.last_y = self.x_loc, self.y_loc
     
-    
-    
+  
 class Game:
   sense = SenseHat()
   space_color = [5, 5, 5]
@@ -111,128 +110,127 @@ class Game:
     return ship_form
   
   
-  def main(): 
-    Game.create_field()
-    enemy_direction = [True, False] #true means shift right and also up, enemies by default go right and down
-    count = 0
+def main(): 
+  Game.create_field()
+  enemy_direction = [True, False] #true means shift right and also up, enemies by default go right and down
+  count = 0
 
-    while(True):
-      for event in Game.sense.stick.get_events(): #using held breaks so far
-        #print(command.direction)
-        if event.action == 'pressed':
-          if event.direction == 'left' and Game.fighters[0].x_loc > Game.x_min:
-            #print('yesl')
-            Game.fighters[0].x_loc-=1
-          elif event.direction == 'right' and Game.fighters[0].x_loc < Game.x_max:
-            #print ('blah')
-            Game.fighters[0].x_loc+=1
-          elif event.direction == 'up':
-            #print ('yes')
-            cur_miss = Game.fighters[0].fire(5)
-            Game.missile_list.append(cur_miss)
-            Game.sense.set_pixel(cur_miss.x_loc, cur_miss.y_loc, cur_miss.color)
-            
-        #elif event.action == 'held':
-        #  print ('held')
-        #  break
-        
-        Game.move(Game.fighters[0])
-      
-      time.sleep(0.01)
-      count+=1
-        
-      if count%50==0:
-        if count == 150: #moves one third of missile auto speed
-          #enemies[0] is the origin of the formation
-          x_left = Game.formation(Game.form, Game.enemies[0].x_loc-1, Game.enemies[0].y_loc)
-          x_right = Game.formation(Game.form, Game.enemies[0].x_loc+1, Game.enemies[0].y_loc)
-          y_up = Game.formation(Game.form, Game.enemies[0].x_loc, Game.enemies[0].y_loc-1)
-          y_down = Game.formation(Game.form, Game.enemies[0].x_loc, Game.enemies[0].y_loc+1)
-          #print(len(x_left), len(x_right), len(y_down))
-          if enemy_direction[0]==True and len(x_right)!=1:
-            shift = x_right
-          elif enemy_direction[0]==True and len(x_right)==1:
-            enemy_direction[0] = not enemy_direction[0]
-            #shift = y_down
-            
-            if enemy_direction[1]==False and len(y_down)!=1:
-              shift = y_down
-            elif enemy_direction[1]==False and len(y_down)==1:
-              enemy_direction[1] = not enemy_direction[1]
-              shift = y_up
-            elif enemy_direction[1]==True and len(y_up)!=1:
-              shift = y_up
-            elif enemy_direction[1]==True and len(y_up)==1:
-              enemy_direction[1] = not enemy_direction[1]
-              shift = y_down
-            
-          elif enemy_direction[0]==False and len(x_left)!=1:
-            shift = x_left
-          elif enemy_direction[0]==False and len(x_left)==1:
-            enemy_direction[0] = not enemy_direction[0]
-            #shift = y_down
-            
-            if enemy_direction[1]==False and len(y_down)!=1:
-              shift = y_down
-            elif enemy_direction[1]==False and len(y_down)==1:
-              enemy_direction[1] = not enemy_direction[1]
-              shift = y_up
-            elif enemy_direction[1]==True and len(y_up)!=1:
-              shift = y_up
-            elif enemy_direction[1]==True and len(y_up)==1:
-              enemy_direction[1] = not enemy_direction[1]
-              shift = y_down
-            
-          for i in range(len(Game.enemies)): #moves the enemy ships
-            #print(shift[i][0], shift[i][1])
-            Game.enemies[i].x_loc, Game.enemies[i].y_loc = shift[i][0], shift[i][1]
-            #cur_ship = Game.enemies[i]
-            
-            if random.randint(0, 3)==1: #fires missile
-              cur_miss = Game.enemies[i].fire(5)
-              Game.sense.set_pixel(cur_miss.x_loc, cur_miss.y_loc, cur_miss.color)
-              Game.missile_list.append(cur_miss)
-             
-            Game.move(Game.enemies[i])
-
-          count = 0
+  while(True):
+    for event in Game.sense.stick.get_events(): #using held breaks so far
+      #print(command.direction)
+      if event.action == 'pressed':
+        if event.direction == 'left' and Game.fighters[0].x_loc > Game.x_min:
+          #print('yesl')
+          Game.fighters[0].x_loc-=1
+        elif event.direction == 'right' and Game.fighters[0].x_loc < Game.x_max:
+          #print ('blah')
+          Game.fighters[0].x_loc+=1
+        elif event.direction == 'up':
+          #print ('yes')
+          cur_miss = Game.fighters[0].fire(5)
+          Game.missile_list.append(cur_miss)
+          Game.sense.set_pixel(cur_miss.x_loc, cur_miss.y_loc, cur_miss.color)
           
-        for missile in Game.missile_list: #moves the missiles
-          if missile.y_loc-1 >= Game.y_min and missile.y_loc+1 <= Game.y_max:
-            missile.y_loc+=missile.modify
-            Game.move(missile)
-          else:
-            Game.sense.set_pixel(missile.x_loc, missile.y_loc, Game.space_color)
-            Game.missile_list.remove(missile)
-            
-        for hit_info in Game.collision():
-          print(hit_info[0].hp, hit_info[1].damage, hit_info[0].enemy)
-          hit_info[0].hp -= hit_info[1].damage
-          Game.missile_list.remove(hit_info[1])
-          if hit_info[0].hp <= 0 and hit_info[0].enemy == True: #only removes if an enemy
-            try:
-              Game.fighters.remove(hit_info[0])
-              Game.enemies.remove(hit_info[0])
-            except:
-              print('yes')
-            Game.sense.set_pixel(hit_info[0].x_loc, hit_info[0].y_loc, hit_info[0].color)
-            #time.sleep(0.1)
-            #Game.sense.set_pixel(hit_info[0].x_loc, hit_info[0].y_loc, Game.space_color)
-            
-        if Game.fighters[0].hp <= 0:
-          Game.sense.show_message("Your ship was destroyed")
-          break
-        elif len(Game.fighters)==1:
-          Game.sense.show_message("All enemies killed, next round")
-          Game.create_field()
-        
-      #sense.set_pixel(ship.last_x, ship.last_y, space_color)
-      #ship.last_x, ship.last_y = ship.x_loc, ship.y_loc
-      #sense.set_pixel(ship.x_loc, ship.y_loc, ship.color)
-      #print('got here')
+      #elif event.action == 'held':
+      #  print ('held')
+      #  break
       
-        
-      #time.sleep(1)
-      
+      Game.move(Game.fighters[0])
     
-Game.main()
+    time.sleep(0.01)
+    count+=1
+      
+    if count%50==0:
+      if count == 150: #moves one third of missile auto speed
+        #enemies[0] is the origin of the formation
+        x_left = Game.formation(Game.form, Game.enemies[0].x_loc-1, Game.enemies[0].y_loc)
+        x_right = Game.formation(Game.form, Game.enemies[0].x_loc+1, Game.enemies[0].y_loc)
+        y_up = Game.formation(Game.form, Game.enemies[0].x_loc, Game.enemies[0].y_loc-1)
+        y_down = Game.formation(Game.form, Game.enemies[0].x_loc, Game.enemies[0].y_loc+1)
+        #print(len(x_left), len(x_right), len(y_down))
+        if enemy_direction[0]==True and len(x_right)!=1:
+          shift = x_right
+        elif enemy_direction[0]==True and len(x_right)==1:
+          enemy_direction[0] = not enemy_direction[0]
+          #shift = y_down
+          
+          if enemy_direction[1]==False and len(y_down)!=1:
+            shift = y_down
+          elif enemy_direction[1]==False and len(y_down)==1:
+            enemy_direction[1] = not enemy_direction[1]
+            shift = y_up
+          elif enemy_direction[1]==True and len(y_up)!=1:
+            shift = y_up
+          elif enemy_direction[1]==True and len(y_up)==1:
+            enemy_direction[1] = not enemy_direction[1]
+            shift = y_down
+          
+        elif enemy_direction[0]==False and len(x_left)!=1:
+          shift = x_left
+        elif enemy_direction[0]==False and len(x_left)==1:
+          enemy_direction[0] = not enemy_direction[0]
+          #shift = y_down
+          
+          if enemy_direction[1]==False and len(y_down)!=1:
+            shift = y_down
+          elif enemy_direction[1]==False and len(y_down)==1:
+            enemy_direction[1] = not enemy_direction[1]
+            shift = y_up
+          elif enemy_direction[1]==True and len(y_up)!=1:
+            shift = y_up
+          elif enemy_direction[1]==True and len(y_up)==1:
+            enemy_direction[1] = not enemy_direction[1]
+            shift = y_down
+          
+        for i in range(len(Game.enemies)): #moves the enemy ships
+          #print(shift[i][0], shift[i][1])
+          Game.enemies[i].x_loc, Game.enemies[i].y_loc = shift[i][0], shift[i][1]
+          #cur_ship = Game.enemies[i]
+          
+          if random.randint(0, 3)==1: #fires missile
+            cur_miss = Game.enemies[i].fire(5)
+            Game.sense.set_pixel(cur_miss.x_loc, cur_miss.y_loc, cur_miss.color)
+            Game.missile_list.append(cur_miss)
+           
+          Game.move(Game.enemies[i])
+
+        count = 0
+        
+      for missile in Game.missile_list: #moves the missiles
+        if missile.y_loc-1 >= Game.y_min and missile.y_loc+1 <= Game.y_max:
+          missile.y_loc+=missile.modify
+          Game.move(missile)
+        else:
+          Game.sense.set_pixel(missile.x_loc, missile.y_loc, Game.space_color)
+          Game.missile_list.remove(missile)
+          
+      for hit_info in Game.collision():
+        print(hit_info[0].hp, hit_info[1].damage, hit_info[0].enemy)
+        hit_info[0].hp -= hit_info[1].damage
+        Game.missile_list.remove(hit_info[1])
+        if hit_info[0].hp <= 0 and hit_info[0].enemy == True: #only removes if an enemy
+          try:
+            Game.fighters.remove(hit_info[0])
+            Game.enemies.remove(hit_info[0])
+          except:
+            print('yes')
+          Game.sense.set_pixel(hit_info[0].x_loc, hit_info[0].y_loc, hit_info[0].color)
+          #time.sleep(0.1)
+          #Game.sense.set_pixel(hit_info[0].x_loc, hit_info[0].y_loc, Game.space_color)
+          
+      if Game.fighters[0].hp <= 0:
+        Game.sense.show_message("Your ship was destroyed")
+        break
+      elif len(Game.fighters)==1:
+        Game.sense.show_message("All enemies killed, next round")
+        Game.create_field()
+      
+    #sense.set_pixel(ship.last_x, ship.last_y, space_color)
+    #ship.last_x, ship.last_y = ship.x_loc, ship.y_loc
+    #sense.set_pixel(ship.x_loc, ship.y_loc, ship.color)
+    #print('got here')
+    
+      
+    #time.sleep(1)
+      
+main()
